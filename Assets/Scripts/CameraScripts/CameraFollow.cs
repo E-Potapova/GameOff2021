56c0cf048f5978a50aab6560fc5d2b72a08963de
ZacharyFredeman <70460310+ZacharyFredeman@users.noreply.github.com>
2021-11-04 00:09:14 -0400

    player movement start

---
+++ Assets/Scripts/CameraScripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    private Vector3 currentVelocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref currentVelocity, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
---
+++ Assets/Scripts/Gameplay/Player.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public WooperController2d controller;
+
+    public float walkspeed = 200f;
+
+    float horizontalMove = 0f;
+
+
+    bool jump = false;
+
+    bool crouch = false;
+
+    bool sprint = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * walkspeed;
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+
+
+        if (Input.GetButtonDown("Sprint"))
+        {
+            sprint = true;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            sprint = false;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (sprint)
+        {
+            controller.Move((horizontalMove * 2) * Time.fixedDeltaTime, crouch, jump);
+        }
+        else if (crouch)
+        {
+            controller.Move((horizontalMove / 2) * Time.fixedDeltaTime, crouch, jump);
+        }
+        else
+        {
+            controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        }
+
+
+
+
+        jump = false;
+    }
+}
