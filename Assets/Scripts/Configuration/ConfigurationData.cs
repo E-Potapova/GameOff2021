0413e1b4168999de03c3ee69366fdaf481758e9a
Renazcel <elizabeth.a.potapova@gmail.com>
2021-11-04 00:26:07 -0400

    basic Events and Config setup

---
+++ Assets/Scripts/Configuration/ConfigurationData.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnityEngine;
+
+/// <summary>
+/// A container for the configuration data
+/// </summary>
+public class ConfigurationData
+{
+    #region Fields
+
+    const string ConfigurationDataFileName = "ConfigData.csv";
+
+    // configuration data
+    float playerSpeed = 10;
+
+    #endregion
+
+    #region Properties
+
+    public float PlayerSpeed
+    {
+        get { return playerSpeed; }
+    }
+
+    #endregion
+
+    #region Constructor
+
+    /// <summary>
+    /// Constructor
+    /// Reads configuration data from a file. If the file
+    /// read fails, the object contains default values for
+    /// the configuration data
+    /// </summary>
+    public ConfigurationData()
+    {
+        StreamReader input = null;
+        try
+        {
+            //open file
+            input = File.OpenText(Path.Combine(
+                Application.streamingAssetsPath,
+                ConfigurationDataFileName));
+            
+            //read values
+            string names = input.ReadLine();
+            string values = input.ReadLine();
+            //read string values into fields
+            SetValuesToFields(values);
+        }
+        catch (Exception e)
+        {
+        }
+        finally
+        {
+            if (input != null)
+            {
+                input.Close();
+            }
+        }
+    }
+
+    #endregion
+
+    void SetValuesToFields(string csvValues)
+    {
+        //split string into array
+        string[] values = csvValues.Split(',');
+
+        //set fields
+        playerSpeed = float.Parse(values[0]);
+    }
+}
---
+++ Assets/Scripts/Configuration/ConfigurationUtils.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Provides access to configuration data
+/// </summary>
+public static class ConfigurationUtils
+{
+    static ConfigurationData configurationData;
+
+    #region Properties
+
+    public static float PaddleMoveUnitsPerSecond
+    {
+        get { return configurationData.PlayerSpeed; }
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Initializes the configuration utils
+    /// </summary>
+    public static void Initialize()
+    {
+        configurationData = new ConfigurationData();
+    }
+}
