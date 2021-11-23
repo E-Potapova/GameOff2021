b2ff43060933ca664f8c0b31d50ad4c0c8629341
Renazcel <elizabeth.a.potapova@gmail.com>
2021-11-23 12:42:43 -0500

    added basic audio system; deleted CSV

---
+++ Assets/Scripts/Audio/AudioClipName.cs
@@ -0,0 +1,13 @@
+/// <summary>
+/// An enumeration of the audio clip names
+/// </summary>
+public enum AudioClipName
+{
+    DirtStep,
+    Jump,
+    Dash,
+    DoubleJump,
+    Hurt,
+    Heal,
+    Death
+}
